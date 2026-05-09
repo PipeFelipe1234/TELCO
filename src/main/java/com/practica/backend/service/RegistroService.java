407d66b592396c4539340327e936155458a44bf6
PipeFelipe1234 <nominapipe@gmail.com>
2026-05-08 20:19:05 -0500

    Filtracion en registros por cargo 2.

--- src/main/java/com/practica/backend/service/RegistroService.java
+++ src/main/java/com/practica/backend/service/RegistroService.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
